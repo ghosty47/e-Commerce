15f82586b6dc5cc0bce218eb4e85da71dce632c7
ghosty47 <ghostcybertron@gmail.com>
2021-10-24 08:46:37 +0100

    added the remaining pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import Home from "./pages/home/Home";
+// import Home from "./pages/home/Home";
+import Cart from "./pages/Cart";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import Product from "./pages/Product";
+// import ProductLists from "./pages/ProductLists";
 
 function App() {
   return (
     <>
-      <Home/>
+      {/* <Home/> */}
+      {/* <ProductLists/> */}
+      {/* <Product/> */}
+      {/* <Register/> */}
+      {/* <Login/> */}
+      <Cart/>
     </>
   );
 }
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,67 @@
+import styled from "styled-components"
+import Announcements from "../components/Announcements"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+
+const Container = styled.div``
+
+const Wrapper = styled.div`
+    padding: 20px;
+`
+
+const Title = styled.h1`
+    font-weight: 300;
+    text-align: center;
+`
+
+const Top= styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const TopButton = styled.button`
+    padding: 10px;
+    font-weight: 600;
+    cursor: pointer;
+    border: ${({type}) => type === "filled" && "none"}
+    background-color: ${({type}) => type === "filled" && "none"}
+    color: ${({type}) => type === "filled" && "none"}
+`
+
+const TopTexts = styled.div``
+const TopText = styled.span`
+    text-decoration: underline;
+    margin: 0px 10px;
+    cursor: pointer;
+`
+
+const Bottom = styled.div``
+
+
+
+
+const Cart = () => {
+    return (
+        <Container>
+            <Navbar/>
+            <Announcements/>
+            <Wrapper>
+                <Title> YOUR BAG </Title>
+                <Top>
+                    <TopButton> CONTINUE SHOPPING </TopButton>
+                    <TopTexts>
+                        <TopText> Shopping Bag(2) </TopText>
+                        <TopText> Your Wishlist(0) </TopText>
+                    </TopTexts>
+                    <TopButton type='filled'> CHECKOUT NOW </TopButton>
+                </Top>
+                <Bottom></Bottom>
+            </Wrapper>
+            <Footer/>
+        </Container>
+    )
+}
+
+export default Cart
